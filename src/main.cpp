6daee46a1cd73e7b3ee3ec40f98f2ee09511018c
ziqing <ZiQingDream@outlook.com>
2025-07-22 08:34:33 +0800

    feat: 主题调用splog完成日志输出，并支持中文utf-8

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 /**
  * \mainpage
  * \par Tanks
- * Gra w czołgi wzorowana na Battle City / Tank 1990 umożliwiająca grę przez jednego lub dwóch graczy
- * \author Krystian Kałużny
+ * Gra w czo?gi wzorowana na Battle City / Tank 1990 umo?liwiaj?ca gr? przez jednego lub dwóch graczy
+ * \author Krystian Ka?u?ny
  * \date 12.05.215
  * \version 1.0
  * \par Kontakt:
@@ -10,11 +10,24 @@
 */
 
 #include "app/app.h"
+#include "spdlog/spdlog.h"
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 int main( int argc, char* args[] )
 {
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+#endif
+
+    spdlog::set_level(spdlog::level::info);
+    spdlog::info("创建游戏主体，准备启动游戏程序");
+
     App app;
     app.run();
 
+    spdlog::info("游戏运行结束，正常退出");
     return 0;
 }
